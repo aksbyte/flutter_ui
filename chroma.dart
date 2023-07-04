cf9a9efdd0b754d3750a0d49ab4b4dbe6dcc83ba
Akshay Nishad <54511621+aksbyte@users.noreply.github.com>
2023-07-04 17:34:43 +0530

    Create chroma.dart
    material3 usage

---
+++ chroma.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
+  static const Color firstBoxColor = Color.fromRGBO(165, 231, 244, 1);
+  static const Color secondBoxColor =
+      Color.fromRGBO(157, 202, 235, 1);
+  static const Color thirdBoxColor = Color.fromRGBO(162, 238, 239, 1);
+  static const Color circleColor = Color.fromRGBO(209, 243, 249, 1);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
